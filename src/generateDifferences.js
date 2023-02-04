85700ab1a430e15a23922cc066c160cfa8f0b9e4
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2023-02-04 16:29:27 +0500

    refactor: improve code clearence

--- src/generateDifferences.js
+++ src/generateDifferences.js
@@ -1,28 +1,18 @@
 import * as fs from 'fs';
 import path from 'path';
-import { getExtension, compareObjects } from './index.js';
-import yamlParser from './parsers.js';
+import { compareObjects } from './index.js';
+import parse from './parsers.js';
 
 const generateDifferences = (filepath1, filepath2) => {
   const fileContent1 = fs.readFileSync(path.resolve(process.cwd(), filepath1));
   const fileContent2 = fs.readFileSync(path.resolve(process.cwd(), filepath2));
 
-  if ((getExtension(filepath1) === 'json') && (getExtension(filepath2) === 'json')) {
-    const obj1 = JSON.parse(fileContent1);
-    const obj2 = JSON.parse(fileContent2);
-    const resultOfComparingObjects = compareObjects(obj1, obj2);
-    return JSON.stringify(resultOfComparingObjects, null, 2).replace(/[",]/g, '');
-  }
+  const obj1 = parse(filepath1, fileContent1);
+  const obj2 = parse(filepath2, fileContent2);
+  const resultOfComparingObjects = compareObjects(obj1, obj2);
 
-  if (((getExtension(filepath1) === 'yaml') || (getExtension(filepath1) === 'yml'))
-  && ((getExtension(filepath2) === 'yaml') || (getExtension(filepath2) === 'yml'))) {
-    const obj1 = yamlParser(fileContent1);
-    const obj2 = yamlParser(fileContent2);
-    const resultOfComparingObjects = compareObjects(obj1, obj2);
-    return JSON.stringify(resultOfComparingObjects, null, 2).replace(/[",]/g, '');
-  }
-
-  return 'not .JSON or . YML';
+  return (JSON.stringify(resultOfComparingObjects, null, 2).replace(/[",]/g, '')
+  || 'not .JSON or .YML');
 };
 
 export default generateDifferences;
--- src/parsers.js
+++ src/parsers.js
@@ -1,4 +1,10 @@
 import yaml from 'js-yaml';
+import { getExtension } from './index.js';
 
-const yamlParser = (file) => yaml.load(file);
-export default yamlParser;
+const parse = (filepath, fileContent) => {
+  if ((getExtension(filepath) === 'json') && (getExtension(filepath) === 'json')) {
+    return JSON.parse(fileContent);
+  } return yaml.load(fileContent);
+};
+
+export default parse;
