8c7087ea2591066946823a95ee8ade9a9f3cf2cd
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2023-02-05 15:51:09 +0500

    feat: add fixtures

--- src/parsers.js
+++ src/parsers.js
@@ -4,7 +4,8 @@ import { getExtension } from './index.js';
 const parse = (filepath, fileContent) => {
   if ((getExtension(filepath) === 'json') && (getExtension(filepath) === 'json')) {
     return JSON.parse(fileContent);
-  } return yaml.load(fileContent);
+  }
+  return yaml.load(fileContent);
 };
 
 export default parse;
