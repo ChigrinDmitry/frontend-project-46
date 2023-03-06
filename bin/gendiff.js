d482d514f0eed5680a253d6984e66659d98a811d
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2023-03-06 22:08:43 +0500

    feat: add choise of formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import { Command } from 'commander';
 import stylish from '../formatters/stylish.js';
 import plain from '../formatters/plain.js';
 import generateDifferences from '../src/generateDifferences.js';
+import { diff } from '../formatters/index.js';
 
 const program = new Command();
 
@@ -19,12 +20,7 @@ program
 program
   .action((filepath1, filepath2) => {
     const options = program.opts();
-    if ((options.format === undefined) || (options.format === 'stylish')) {
-      console.log(stylish(generateDifferences(filepath1, filepath2), ' ', 4));
-    }
-    if (options.format === 'plain') {
-      console.log(plain(generateDifferences(filepath1, filepath2)));
-    }
+    console.log(diff(filepath1, filepath2, options.format))
   });
 
 program.parse(process.argv);
--- formatters/index.js
+++ formatters/index.js
@@ -1,3 +1,16 @@
+import stylish from '../formatters/stylish.js';
+import plain from '../formatters/plain.js';
+import generateDifferences from '../src/generateDifferences.js';
+
 export const normalizeQuotes = (value) => {
   return typeof(value) === 'string' ? `'${value}'` : `${value}`;
+};
+
+export const diff = (filepath1, filepath2, formatName) => {
+  if (formatName === 'plain') {
+    return plain(generateDifferences(filepath1, filepath2))
+  }
+  if (formatName === 'stylish') {
+    return stylish(generateDifferences(filepath1, filepath2), ' ', 4);
+  }
 };
